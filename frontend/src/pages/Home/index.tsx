533ba90801715de3f33c460432cbe850667a87b2
sumaeta <brenosumaeta33@gmail.com>
2021-05-12 11:21:24 -0400

    Primeira Requisição

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -9,7 +9,7 @@ const Home = () => {
         <div className="container">
             <div className="jumbotron">
                 <h1 className="display-4">DSVendas</h1>
-                <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                <p className="lead">Análise o desempenho das suas vendas por diferentes perspectivas</p>
                 <hr />
                 <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
             <Link className="btn btn-primary btn-lg" to="/dashboard"> 
