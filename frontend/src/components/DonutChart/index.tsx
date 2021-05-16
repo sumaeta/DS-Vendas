a458f7eda33988290d6195b20678baa6e279eacf
sumaeta <brenosumaeta33@gmail.com>
2021-05-16 12:22:52 -0400

    finalizando o gráfico de barra

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -4,7 +4,6 @@ import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
-
 type ChartData = {
     labels: string[];
     series: number[];
@@ -20,18 +19,11 @@ const DonutChart = () => {
         .then(response => {
             const data = response.data as SaleSum[];
             const myLabels = data.map(x => x.sellerName);
-            const mySiries = data.map(x => x.sum);
+            const mySeries = data.map(x => x.sum);
 
-            setChartData({labels: myLabels, series: mySiries});
+            setChartData({labels: myLabels, series: mySeries});
         });
     }, []);
-
-    
-
-    //const mockData = {
-    //    series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
 
     const options = {
         legend: {
